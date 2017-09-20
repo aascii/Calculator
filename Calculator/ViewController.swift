da8c7c166f45ba429288dc5f5ce97b32662b4fcb
Jason Hayes <ascii@Shelley-Moon-Photography.local>
2017-09-20 10:53:52 -0500

    lesson 2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,22 +14,23 @@ class ViewController: UIViewController {
     
     var userIsInTheMiddleOfTyping = false
     
-    let displayLength = 9
+    let displayLength = 16
     
     var displayEntries = 0
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        if displayEntries < displayLength {
-            if userIsInTheMiddleOfTyping {
+        
+        if userIsInTheMiddleOfTyping {
+            if displayEntries < displayLength {
                 let textCurrentlyInDisplay = display.text!
                 display.text = textCurrentlyInDisplay + digit
-            } else {
-                display.text = digit
-                userIsInTheMiddleOfTyping = true
             }
-            displayEntries += 1
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
         }
+        displayEntries += 1
     }
     
     var displayValue: Double {
@@ -41,19 +42,19 @@ class ViewController: UIViewController {
         }
     }
     
+    private var brain: CalculatorBrain = CalculatorBrain()
+    
     @IBAction func performOperation(_ sender: UIButton) {
-        userIsInTheMiddleOfTyping = false
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
         if let mathematicalSymbol = sender.currentTitle {
-            switch mathematicalSymbol {
-            case "π":
-                displayValue = Double.pi
-                displayEntries = 9
-            case "√":
-                displayValue = sqrt(displayValue)
-                displayEntries = 9
-            default:
-                break
-            }
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+            displayEntries = 0
         }
     }
     
