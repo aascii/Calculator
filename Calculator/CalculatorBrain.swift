020beb377473395db7a6cb163a53f2cf1b895a1d
Jason Hayes <ascii@Shelley-Moon-Photography.local>
2017-09-22 15:10:19 -0500

    operand/operator history log, a few new functions, and a reset (Clear) button

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,16 +10,20 @@ import Foundation
 
 struct CalculatorBrain {
     
-    private var accumulator: Double?
+    /// Latest result of calculation along with a history of operands and operations
+    private var accumulator: (value: Double?,history: String) = (nil, "")
     
+    /// Types of things the calculator can perform
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double,Double) -> Double)
         case equals
+        case clearHistory
     }
     
-    var resultIsPending = false
+    /// Stores "..." while in the middle of an operation; otherwise nil
+    var resultIsPending: String?
     
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.constant(Double.pi),
@@ -35,45 +39,71 @@ struct CalculatorBrain {
         "÷" : Operation.binaryOperation({ $0 / $1 }),
         "+" : Operation.binaryOperation({ $0 + $1 }),
         "−" : Operation.binaryOperation({ $0 - $1 }),
-        "=" : Operation.equals
+        "=" : Operation.equals,
+        "∁" : Operation.clearHistory
     ]
     
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
-            case .constant(let value):
-                accumulator = value
+            case .constant(let numericalValue):
+                accumulator.value = numericalValue
+                if resultIsPending != nil {
+                    accumulator.history += symbol
+                } else {
+                    accumulator.history = symbol
+                }
             case .unaryOperation(let function):
-                if accumulator != nil {
-                    accumulator = function(accumulator!)
+                if accumulator.value != nil {
+                    if resultIsPending != nil {
+                        let charsToRemove = String(accumulator.value!).count
+                        let rangeToRemove = accumulator.history.index(accumulator.history.endIndex, offsetBy: -charsToRemove)..<accumulator.history.endIndex
+                        accumulator.history.removeSubrange(rangeToRemove)
+                        accumulator.history = accumulator.history + String(symbol) + "(" + String(accumulator.value!) + ")"
+                    } else {
+                        accumulator.history = String(symbol) + "(" + accumulator.history + ")"
+                    }
+                    accumulator.value = function(accumulator.value!)
                 }
             case .binaryOperation(let function):
-                if accumulator != nil {
-                    if resultIsPending {
+                if accumulator.value != nil {
+                    if resultIsPending != nil {
                         performPendingBinaryOperation()
                     }
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
-                    if resultIsPending == false {
-                        accumulator = nil
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator.value!)
+                    if symbol == "×" || symbol == "÷" {
+                        accumulator.history = "(" + accumulator.history + ")" + String(symbol)
+                    } else {
+                        accumulator.history += String(symbol)
                     }
-                    resultIsPending = true
+                    resultIsPending = "..."
                 }
             case .equals:
                 performPendingBinaryOperation()
-                resultIsPending = false
+                resultIsPending = nil
+            case .clearHistory:
+                accumulator = (nil, "")
+                resultIsPending = nil
+                pendingBinaryOperation = nil
             }
         }
     }
     
+    /// Returns result of pending binary operation:
+    /// Runs ALU clock on the pending operation with contents from
+    /// pending operation struct and accumulator;
+    /// Closes operation by clearing pending status
     private mutating func performPendingBinaryOperation() {
-        if pendingBinaryOperation != nil && accumulator != nil {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+        if pendingBinaryOperation != nil && accumulator.value != nil {
+            accumulator.value = pendingBinaryOperation!.perform(with: accumulator.value!)
             pendingBinaryOperation = nil
         }
     }
     
+    /// Stores pending operator with its first operand
     private var pendingBinaryOperation: PendingBinaryOperation?
     
+    /// Collection including a pending operator and its first operand
     private struct PendingBinaryOperation {
         let function: (Double,Double) -> Double
         let firstOperand: Double
@@ -83,13 +113,25 @@ struct CalculatorBrain {
         }
     }
     
+    /// Sets the accumulator with passed-in operand and records in historical log
     mutating func setOperand(_ operand: Double) {
-        accumulator = operand
+        accumulator.value = operand
+        if resultIsPending != nil {
+            accumulator.history += String(operand)
+        } else {
+            accumulator.history = String(operand)
+        }
     }
     
     var result: Double? {
         get {
-            return accumulator
+            return accumulator.value
+        }
+    }
+    
+    var history: String? {
+        get {
+            return accumulator.history
         }
     }
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    /// Element used for operand input as well as displaying operation results
+    /// Note: AKA "input UI" || "results UI"
     @IBOutlet weak var display: UILabel!
+    /// Element used for displaying the running historical log of operand and operations
+    /// Note: AKA "history UI"
+    @IBOutlet weak var inputHistoryDisplay: UILabel!
     
     var userIsInTheMiddleOfTyping = false
     
+    /// Character limit of "input" UI
     let displayLength = 16
-    
+    /// Element tracking current "input" operand string length
     var displayEntries = 0
     
+    /// Catches and handles numerical button actions in preparation to set operand
+    /// Note:  Does NOT modify brain properties
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         
@@ -34,34 +42,66 @@ class ViewController: UIViewController {
                 display.text = digit
                 userIsInTheMiddleOfTyping = true
                 displayEntries += 1
+            } else {
+                display.text = "0" + digit
+                userIsInTheMiddleOfTyping = true
+                displayEntries += 2
             }
         }
     }
     
     var displayValue: Double {
+        // Returns the prepared operand value from "input" UI
         get {
             return Double(display.text!)!
         }
+        // Refreshes the "result" UI with updated value
+        // Note: this is the action which triggers the screen refresh
         set {
             display.text = String(newValue)
         }
     }
     
+    var displayHistoryValue: String {
+        // Returns the prepared historical log
+        get {
+            return inputHistoryDisplay.text!
+        }
+        // Refreshes the "history" UI with updated value
+        // Note: this is the action which triggers the screen refresh
+        set {
+            inputHistoryDisplay.text = newValue
+        }
+    }
+    
+    /// Create calculator ALU, including the accumulator
     private var brain: CalculatorBrain = CalculatorBrain()
     
+    /// Catches and handles all non-numerical button actions, eg:
+    /// constants, operators, reset (AKA Clear button)
     @IBAction func performOperation(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping {
+            // store a new value in the accumulator
             brain.setOperand(displayValue)
             userIsInTheMiddleOfTyping = false
         }
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
+        // if the accumulator is empty, reset the calculator to start state
+        // otherwise, since we are at the end of performOperation,
+        //     trigger the screen refresh(es)
         if let result = brain.result {
             displayValue = result
+            displayHistoryValue = brain.history! + (brain.resultIsPending ?? "=")
+            displayEntries = 0
+        } else {
+            display.text = "0"
+            displayHistoryValue = "0"
             displayEntries = 0
         }
     }
+    
     
 }
 
