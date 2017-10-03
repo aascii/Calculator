aascii/Calculator

commit 00abebc2744ce3506e24df3d2106d57d3dbf5cfa

swift 3 does not support String.suffix() - now uses String.substring(from: )

Calculator/ViewController.swift
@@ -197,7 +197,8 @@ class ViewController: UIViewController {
     
     @IBAction func setMathVariableValue(_ sender: UIButton) {
         let button = sender.currentTitle!
-        let mathVariable = String(button.suffix(1))
+        let mathVariable = String(button.substring(from:
+            button.index(button.endIndex, offsetBy: -2)))
         
         if userIsInTheMiddleOfTyping {
             userIsInTheMiddleOfTyping = false
@@ -205,7 +206,7 @@ class ViewController: UIViewController {
         
         displayMathVariableValue = displayValue
 
-        let results = brain.evaluate(using: [mathVariable: displayValue])
+        let results = brain.evaluate(using: [mathVariable!: displayValue])
         
         // if brain stack is empty, reset the calculator display to start state.
         // otherwise, trigger the screen refresh(es)

Calculator/CalculatorBrain.swift
@@ -203,7 +203,7 @@ struct CalculatorBrain {
                                             switch previousEntry {
                                             case .remove:
                                                 // should never be here!
-                                                description = "error!"
+                                                description = "'undo' bug!"
                                                 break
                                             case .opcode(let previousString):
                                                 lastEntry = previousString
@@ -309,7 +309,7 @@ struct CalculatorBrain {
                 formatTextFromStack.string(from: formattedTextNumber)
             return formattedText!
         } else {
-            return "error"
+            return "format bug!"
         }
     }
     

commit b7309a570a6b64e8f45bae5936d9d1f67f60246b

fix calculator memory access bug introduced in commit 00abebc, and change how memory contents display is reset during calculator "clear" operation

Calculator/Base.lproj/Main.storyboard
@@ -1,11 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="13196" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES" initialViewController="BYZ-38-t0r">
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="11762" systemVersion="15G1611" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES" initialViewController="BYZ-38-t0r">
     <device id="retina4_7" orientation="portrait">
         <adaptation id="fullscreen"/>
     </device>
     <dependencies>
         <deployment identifier="iOS"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="13173"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="11757"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
     </dependencies>
     <scenes>
@@ -52,8 +52,8 @@
                                                             <color key="titleColor" white="1" alpha="1" colorSpace="calibratedWhite"/>
                                                         </state>
                                                         <connections>
+                                                            <action selector="clearMemoryDisplay:" destination="BYZ-38-t0r" eventType="touchUpInside" id="tgO-c7-vxw"/>
                                                             <action selector="performOperation:" destination="BYZ-38-t0r" eventType="touchUpInside" id="6LR-ch-iB4"/>
-                                                            <action selector="setMathVariableValue:" destination="BYZ-38-t0r" eventType="touchUpInside" id="uuE-ig-bq4"/>
                                                         </connections>
                                                     </button>
                                                     <button opaque="NO" contentMode="scaleToFill" contentHorizontalAlignment="center" contentVerticalAlignment="center" buttonType="roundedRect" lineBreakMode="middleTruncation" translatesAutoresizingMaskIntoConstraints="NO" id="ecy-cM-t3E">

Calculator/ViewController.swift
@@ -198,7 +198,7 @@ class ViewController: UIViewController {
     @IBAction func setMathVariableValue(_ sender: UIButton) {
         let button = sender.currentTitle!
         let mathVariable = String(button.substring(from:
-            button.index(button.endIndex, offsetBy: -2)))
+            button.index(button.endIndex, offsetBy: -1)))
         
         if userIsInTheMiddleOfTyping {
             userIsInTheMiddleOfTyping = false
@@ -228,6 +228,10 @@ class ViewController: UIViewController {
         // otherwise, since we are at the end of evaluation,
         //     trigger the screen refresh(es)
         screenRefresh(results)
+    }
+    
+    @IBAction func clearMemoryDisplay(_ sender: UIButton) {
+        displayMathVariableValue = 0
     }
     
     private func screenRefresh(_ results: